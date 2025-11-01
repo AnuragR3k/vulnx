0e59879809ffc3f657831a16a84221b9fd424d95
AnuragR3k <anuragrag9890@gmail.com>
2025-11-01 13:19:01 +0530

    Fix font path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  // 👇 Add this INSIDE defineConfig
   assetsInclude: ['**/*.otf', '**/*.ttf', '**/*.woff', '**/*.woff2'],
-
-  base: process.env.VITE_BASE_PATH || '/vulnx',
-
+  base: '/',   // ✅ CHANGE THIS
   server: {
     port: 5173,
     proxy: {
@@ -20,4 +15,4 @@ export default defineConfig({
       },
     },
   },
-});
+})
